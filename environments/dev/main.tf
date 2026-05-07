b54453c9d63f0dcbb5cca210bd37818786e5e779
devijavvadi <devi.javvadi@gmail.com>
2026-05-06 23:50:54 -0400

    added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,6 +2,6 @@ module "ec2" {
   source = "../../modules/ec2"
 
   instance_type = var.instance_type
-  env           = var.environment
+  environment   = var.environment
   project       = var.project
 }

5edbc3781d6f2111c586337f4b0e6cb71fcd4c79
devijavvadi <devi.javvadi@gmail.com>
2026-05-07 11:59:33 -0400

    vpc module is ready

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,3 +5,13 @@ module "ec2" {
   environment   = var.environment
   project       = var.project
 }
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  environment          = var.environment
+}

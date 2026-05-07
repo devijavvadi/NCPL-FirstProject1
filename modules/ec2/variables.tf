184962ddfdf6d7b7a8185c0daa7cada7c8066417
devijavvadi <devi.javvadi@gmail.com>
2026-05-06 20:48:40 -0400

    provider.tf error rectified

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "iam_instance_profile" {
+  type = string
+}

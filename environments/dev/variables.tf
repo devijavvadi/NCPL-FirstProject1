84246644dfb88abdbfd862ec9665ddec6f20fd2f
devijavvadi <devi.javvadi@gmail.com>
2026-05-07 12:17:44 -0400

    vpc module is ready

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -13,3 +13,21 @@ variable "environment" {
 variable "aws_region" {
   type = string
 }
+
+variable "vpc_cidr" {
+  type = string
+}
+
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+ type        = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -4,15 +4,15 @@ variable "vpc_cidr" {
 
 
 variable "public_subnet_cidrs" {
-  type        = list(string)
+  type = list(string)
 }
 
 variable "private_subnet_cidrs" {
- type        = list(string)
+ type = list(string)
 }
 
 variable "availability_zones" {
-  type = string
+  type = list(string)
 }
 
 variable "environment" {

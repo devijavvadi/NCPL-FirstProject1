a212e10f08252b80cf6c384170431854604f013e
devijavvadi <devi.javvadi@gmail.com>
2026-05-08 14:53:25 -0400

    refreshed

---
+++ modules/iam/variables.tf
@@ -0,0 +1,15 @@
+variable "user_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "policy_json" {
+  type = string
+}

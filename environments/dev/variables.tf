2f90d7cb081c78a64c220eedcec786f5e2c061db
devijavvadi <devi.javvadi@gmail.com>
2026-05-07 21:29:45 -0400

    hm hurayyy

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -20,14 +20,21 @@ variable "vpc_cidr" {
 }
 
 
+
 variable "public_subnet_cidrs" {
   type = list(string)
-
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
+  # No default value here
 }
+
+
+//variable "public_subnet_cidrs" {
+//  type = list(string)
+
+ // default = [
+ //   "10.0.1.0/24",
+ //   "10.0.2.0/24"
+//  ]
+//} 
 
 
 variable "private_subnet_cidrs" {
